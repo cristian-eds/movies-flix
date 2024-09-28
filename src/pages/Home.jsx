237669ebff20b654b8622868b816bd812b33bb26
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-09-28 20:32:22 -0300

    Criado MovieCard estrutura

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react"
+import Movie from "./Movie";
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -20,8 +22,12 @@ const Home = () => {
     },[])
 
     return (
-        <div>
-            {topMovies && topMovies.map((movie) =>  <p>{movie.title}</p> )}
+        <div className="container">
+            <h2 className="title">Melhores filmes:</h2>
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando...</p> }
+                {topMovies.length > 0 && topMovies.map((movie) =>  <MovieCard movie={movie} key={movie.id}/> )}
+            </div>
         </div>
     )
 }
