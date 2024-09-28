da253b2b2b228f85b2ce21820b05234c7e8b9afd
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-09-27 22:11:26 -0300

    Configurando react router no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={ <App />}>
+          <Route path='/' element={<Home />}/>
+          <Route path='/movie/:id' element={<Movie />}/>
+          <Route path='/search' element={<Search />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+   
   </StrictMode>,
 )
