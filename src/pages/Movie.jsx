07179e45d50e31ae488893bba4376fc2fda74fac
Cristian Eduardo <cristian.santos0308@gmail.com>
2024-09-28 23:48:24 -0300

    Criado pagina Movie e sua estrutura com detalhes do filme escolhido

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,6 +1,83 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  BsGraphUp, BsWallet, BsHourglassSplit, BsFillFileEarmarkTextFill,
+  BsWallet2
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "./Movie.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
+
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const formatCurrency = (number) => {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
+    })
+  }
+
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovie(data);
+  }
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`;
+    getMovie(movieUrl);
+},[])
+
+
   return (
-    <div>Movie</div>
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false}></MovieCard>
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 /> Orçamento:
+            </h3>
+            <p>
+              {formatCurrency(movie.budget)}
+            </p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsGraphUp /> Receita:
+            </h3>
+            <p>
+              {formatCurrency(movie.revenue)}
+            </p>
+          </div>
+          <div className="info">
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>
+              {movie.runtime}
+            </p>
+            <div className="info">
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>
+              {movie.overview}
+            </p>
+          </div>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
